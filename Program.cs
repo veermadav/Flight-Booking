824ea2d2cb34345e6675d5bd7e17114c42d7370c
kathir-kevin <kkathiravan709@gmail.com>
2023-06-24 20:37:52 +0530

    Flight-Ticket Booking In .Net Core Web Api 6

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ using CorePush.Apple;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
